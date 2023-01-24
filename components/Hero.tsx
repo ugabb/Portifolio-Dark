a483b51ff9cf2ba8ef571256da72112cc364f16a
ugabb <gsb.alternativo@gmail.com>
2023-01-24 18:50:11 -0300

    Portifolio - header, hero sections

---
+++ components/Hero.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Image from "next/image";
+type Props = {};
+
+//react simple typewriter
+import { useTypewriter } from "react-simple-typewriter";
+import { Cursor } from "react-simple-typewriter";
+
+// components
+import BackgroundCircles from "./BackgroundCircles";
+import Link from "next/link";
+
+const Hero = (props: Props) => {
+  const [text, count] = useTypewriter({
+    words: ["Hi, my is Gabriel", "Dev", "Flamengo"],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <div className="h-screen flex flex-col space-y-8 item-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <Image
+        src={"/../public/me.jpg"}
+        width={32}
+        height={32}
+        alt={"uGabDev picture"}
+        className="relative rounded-full h-32 w-32 object-cover mx-auto"
+      />
+
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 tracking-[15px]">
+          Softwate Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold scroll-px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#f7ab0a" />
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="hero-button">About</button>
+          </Link>
+
+          <Link href="#experience">
+            <button className="hero-button">Experiece</button>
+          </Link>
+          <Link href="#skills">
+            <button className="hero-button">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="hero-button">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
