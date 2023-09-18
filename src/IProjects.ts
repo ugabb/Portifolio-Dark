3218324745c7e1bf5d56a18d3bdff453f158bbca
Gabriel Silva Barros <gabriel.barros@mma.gov.br>
2023-09-18 13:51:05 -0300

    feat: added an interface to projects

---
+++ src/IProjects.ts
@@ -0,0 +1,11 @@
+export interface IProjects {
+    name: string
+    photo: string
+    description: string
+    technology1: string
+    technology2: string
+    technology3?: string
+    technology4?: string
+    repo: string
+    more: string
+}
