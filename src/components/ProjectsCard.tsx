b7e0abe8f359a000c7d2333789c94ee877e6d03c
Gabriel Silva Barros <08175460148@mma.gov.br>
2023-11-29 09:16:46 -0300

    ➕feat(projectsCard): new hover effects

--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -16,14 +16,15 @@ const ProjectsCard = (props: Props) => {
     setProjectHover(false)
   }
   return (
-    <div className='w-[600px] flex flex-col justify-center items-center relative cursor-pointer'>
+    <motion.div
+      className='w-[600px] flex flex-col justify-center items-center relative cursor-pointer transition-all hover:scale-[1.02]'>
 
       {projectHover && (
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className='center z-10  pointer-events-none'>
+          className='project-border center '>
           <div className="flex justify-center items-center gap-3 max-w-sm mx-auto">
             <img className='h-10 w-10' src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg' />
             <img className='h-10 w-10' src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg' />
@@ -31,7 +32,11 @@ const ProjectsCard = (props: Props) => {
             <img className='h-10 w-10' src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg' />
           </div>
 
-          <h1 className='text-wild-world text-6xl text-white  text-center'>DEV-SPOTLIGHT</h1>
+          <motion.h1
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0 }}
+            className='text-wild-world text-6xl text-white  text-center'>DEV-SPOTLIGHT</motion.h1>
         </motion.div>
       )}
 
@@ -39,10 +44,10 @@ const ProjectsCard = (props: Props) => {
         onHoverStart={handleProjectShow}
         onHoverEnd={handleProjectHide}
       >
-        <Image className={`rounded-xl transition-all ease-in-out delay-75  ${projectHover ? 'brightness-50 shadow-white shadow-md' : 'opacity-100'}`} src={'/dev-spotlight.png'} width={700} height={470} alt='project image' />
+        <Image className={`rounded-xl transition-all ease-in-out delay-75  ${projectHover ? 'brightness-50 shadow-white/20 shadow-md' : 'opacity-100'}`} src={'/dev-spotlight.png'} width={700} height={470} alt='project image' />
       </motion.div>
 
-    </div>
+    </motion.div>
   )
 }
 
