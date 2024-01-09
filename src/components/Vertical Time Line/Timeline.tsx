c6c4a12c36337a827f169eeaabdbf9f3127ff7b0
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-01-09 14:54:35 -0300

    update 2 files and delete 1 file

--- src/components/Vertical Time Line/Timeline.tsx
+++ src/components/Vertical Time Line/Timeline.tsx
@@ -24,7 +24,7 @@ const Timeline = (props: Props) => {
             </VerticalTimelineElement>
             <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                contentStyle={{ background: '#c2d9f8', color: '#000' }}
+                contentStyle={{ background: '#c2d9f8', color: '#e7e7e7' }}
                 contentArrowStyle={{ borderRight: '7px solid  rgb(147 197 253)' }}
                 date="2023 - atual"
                 iconStyle={{ background: 'rgb(147 197 253)', color: '#fff' }}
@@ -36,7 +36,7 @@ const Timeline = (props: Props) => {
             </VerticalTimelineElement>
             <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                contentStyle={{ background: '#c2d9f8', color: '#000' }}
+                contentStyle={{ background: '#c2d9f8', color: '#e7e7e7' }}
                 contentArrowStyle={{ borderRight: '7px solid  rgb(147 197 253)' }}
                 date="2023 - atual"
                 iconStyle={{ background: 'rgb(147 197 253)', color: '#fff' }}
@@ -46,6 +46,7 @@ const Timeline = (props: Props) => {
                 <p className='text-zinc-700'>Desenvolvimento web com Angular e Spring Boot para o Ministério do Meio Ambiente gerenciar projetos ambientais nacionais e internacionais.</p>
                 <Link className='flex items-center gap-1 text-blue-500 font-semibold hover:underline' href={"https://geprod.mma.gov.br/"} target='_blank'>Geprod <GoLinkExternal /></Link>
             </VerticalTimelineElement>
+    
 
         </VerticalTimeline>
     )
