ca6961235ce6a267c0813b3095251c6ef50bdaa9
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-01-09 14:53:26 -0300

    feat(Shadcn): adding shadcn library

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,14 @@
 
 import { Html, Head, Main, NextScript } from "next/document";
-import { usePathname } from "next/navigation";
+
 
 export default function Document() {
-  const pathName = usePathname()
-  console.log(pathName)
   return (
     <Html lang="en" className="bg-[rgb(36,36,36)]">
       <Head />
       <body>
         <Main />
+
         <NextScript />
       </body>
     </Html>
